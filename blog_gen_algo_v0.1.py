055290b616b3d9f10ebf607db66370f4f4145474
rbatista191 <ricardo.serodio.batista@gmail.com>
2024-01-05 11:40:13 +0100

    Refined GPT prompts

--- blog_gen_algo_v0.1.py
+++ blog_gen_algo_v0.1.py
@@ -25,37 +25,32 @@ steps_prompts = [
     "and teaser in paragraph format.",
     # Step 2
     "Step 2: On the basis of the user intent for asking {primary_keywords}, set up a base ground of knowledge. "
-    "Write facts and theories on this topic, add well-known data points and sources here."
-    "Use maximum 300 words for the content. Don't reach any conclusion yet. "
+    "Write facts and theories on this topic, add well-known data points and sources here. "
+    "Use maximum 250 words for the content. Don't reach any conclusion yet. "
     "\nMake sure to write in pure markdown format, with headings and subheadings (H2 to H3), "
     "paragraphs, lists and text formating (such as bold, italic, strikethrough, etc)."
     "\nLink 2-3 other of my blog posts (found in the sitemap posted below) within the content. "
     "Make sure to sound natural when linking to other blog posts, i.e., the text can only be slightly altered to accommodate a better context for the link. "
-    "Make sure to use the anchor text should not be the actual title of the other blog post, but rather something in the text that makes sense. "
+    "Make sure to use the anchor text is be the actual title of the other blog post, but rather something in the text that goes along the rationale. "
     "Sitemap: {sitemap_urls}",
     # Step 3
-    "Step 3: On the basis of the user intent for asking {primary_keywords}, describe the problem the user is facing "
-    "and give several solutions for it. The solutions cam be either be a process or a product or a service."
-    "Use maximum 300 words for the content. Don't reach any conclusion yet."
+    "Step 3: If applicable, explain step by step how to do the required actions for the user intent in {primary_keywords}. "
+    "Use maximum 400 words for the content. Don't reach any conclusion yet."
     "Make sure to write in pure markdown format, with headings and subheadings (H2 to H3), "
     "paragraphs, lists and text formating (such as bold, italic, strikethrough, etc).",
     # Step 4
-    "Step 4: Introduce {service_name}, {service_description}"
+    "Step 4: Introduce {service_name}, described as {service_description}"
     "Explain to the user how {service_name} can help them with their problem. "
     "Make sure to link {service_url} in the content. "
+    "Demonstrate how to use {service_name} in easy steps. Don't go beyond what is mentioned in the service description. "
     "Use maximum 100 words for the content. Don't reach any conclusion yet. "
     "Make sure to write in pure markdown format, with headings and subheadings (H2 to H3), "
     "paragraphs, lists and text formating (such as bold, italic, strikethrough, etc).",
     # Step 5
-    "Step 5: If applicable, demonstrate how to use {service_name} in easy steps. Don't go beyond what is mentioned in the service description: {service_description}. "
-    "Use maximum 100 words for the content. Don't reach any conclusion yet. "
-    "Make sure to write in pure markdown format, with headings and subheadings (H2 to H3), "
-    "paragraphs, lists and text formating (such as bold, italic, strikethrough, etc).",
-    # Step 6
-    "Step 6: Generate a conclusion based on the content of this blog. Use {tone_of_writing} tone to"
+    "Step 5: Generate a conclusion based on the content of this blog. Use {tone_of_writing} tone to"
     "ease the user intent to take the next step on {primary_keywords}. "
     "Use maximum 150 words for the content."
-    "Make sure to write in pure markdown format, with headings and subheadings (H2 to H3), "
+    "Make sure to write in pure markdown format, with headings and subheadings (H1 to H4), "
     "paragraphs, lists and text formating (such as bold, italic, strikethrough, etc).",
 ]
 
